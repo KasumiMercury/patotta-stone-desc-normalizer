2a8bac365e3f55b3b443cd871752a8f2f107e0cf
KasumiMercury <naoki_shira@outlook.jp>
2024-05-27 14:36:53 +0900

    :construction: parse csv

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+extern crate csv;
+
 use std::error::Error;
 use std::fs::File;
 
@@ -13,6 +15,15 @@ fn greet(name: &str) -> String {
 fn fopen(path: &str) -> Result<File, Box<dyn Error>>{
     let file = File::open(path)?;
     Ok(file)
+}
+
+fn csv_parse(file: File) -> Result<(), Box<dyn Error>> {
+    let mut rdr = csv::Reader::from_reader(file);
+    for result in rdr.records() {
+        let record = result?;
+        println!("{:?}", record);
+    }
+    Ok(())
 }
 
 fn main() {
