2000c053fe7fa1a74fd21caebe226b24b8f91bab
KasumiMercury <naoki_shira@outlook.jp>
2024-05-27 14:35:08 +0900

    :construction: file open

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,18 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::error::Error;
+use std::fs::File;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+fn fopen(path: &str) -> Result<File, Box<dyn Error>>{
+    let file = File::open(path)?;
+    Ok(file)
 }
 
 fn main() {
