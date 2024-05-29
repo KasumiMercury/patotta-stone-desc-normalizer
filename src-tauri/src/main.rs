d28c8f19ba14e529ff74700b0c6e47b852b477c5
KasumiMercury <naoki_shira@outlook.jp>
2024-05-29 12:24:32 +0900

    :construction: remove extern crate

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,13 +1,13 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-mod custom_error;
-
-extern crate csv;
-
 use std::fs::File;
+
 use anyhow::{anyhow, Context as _, Result};
+
 use custom_error::CustomError;
+
+mod custom_error;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -15,7 +15,7 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-fn file_open(path: &str) -> Result<File, CustomError>{
+fn file_open(path: &str) -> Result<File, CustomError> {
     let file = File::open(path)
         .map_err(|e| CustomError::Anyhow(anyhow!("IO Error: {}", e)))
         .with_context(|| format!("Failed to open file: {}", path))?;
