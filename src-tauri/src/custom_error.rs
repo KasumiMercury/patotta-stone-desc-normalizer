87194bc1ec1f82ad1b8b57fa3b85956e6fbdf4e9
KasumiMercury <naoki_shira@outlook.jp>
2024-05-29 11:57:17 +0900

    :construction: remove unused define

--- src-tauri/src/custom_error.rs
+++ src-tauri/src/custom_error.rs
@@ -2,9 +2,5 @@
 #[derive(Debug, thiserror::Error)]
 pub enum CustomError {
     #[error(transparent)]
-    Anyhow(#[from] anyhow::Error),
-    #[error("IO Error: {0}")]
-    IoError(std::io::Error),
-    #[error("CSV Error: {0}")]
-    CsvReaderError(csv::Error),
+    Anyhow(#[from] anyhow::Error)
 }
