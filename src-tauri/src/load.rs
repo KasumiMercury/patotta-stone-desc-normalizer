d6989c98816de9c6d3aafde15d2522e74d3cbcc2
KasumiMercury <naoki_shira@outlook.jp>
2024-06-17 23:49:30 +0900

    :construction: create test mod

--- src-tauri/src/load.rs
+++ src-tauri/src/load.rs
@@ -4,45 +4,11 @@ use serde::Deserialize;
 use sqlx::SqlitePool;
 use crate::custom_error::CustomError;
 
-#[cfg(test)]
-use tempfile::TempDir;
-#[cfg(test)]
-use std::io::Write;
-
 pub(super) fn file_open(path: &str) -> Result<File, CustomError> {
     let file = File::open(path)
         .map_err(|e| CustomError::Anyhow(anyhow!("Failed to open {}: {}", path, e)))?;
     Ok(file)
 }
-
-#[test]
-fn test_file_open_success() {
-    // create a temporary file
-    let dir = TempDir::new().unwrap();
-    let path = dir.path().join("test_file_open.csv");
-
-    let mut temp_file = File::create(&path).unwrap();
-    temp_file.write_all(b"source_id,title,description,published_at,actual_start_at\n").unwrap();
-
-    // open the file test
-    let file = file_open(path.to_str().unwrap()).unwrap();
-
-    // check if the file is opened
-    assert!(file.metadata().is_ok());
-
-    // cleanup
-    std::fs::remove_file(path).unwrap();
-}
-
-#[test]
-fn test_file_open_failure() {
-    // open the file test
-    let file = file_open("non_existent_file.csv");
-
-    // check if the file is opened
-    assert!(file.is_err());
-}
-
 
 #[derive(Deserialize)]
 pub(crate) struct Record {
@@ -90,4 +56,41 @@ pub(super) async fn initialize_desc_table_by_records(
             .map_err(|e| CustomError::Anyhow(anyhow!("Failed to insert into desc: {}", e)))?;
     }
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs::File;
+    use std::io::Write;
+    use tempfile::TempDir;
+    use crate::load::file_open;
+
+    #[test]
+    fn test_file_open_success() {
+        // create a temporary file
+        let dir = TempDir::new().unwrap();
+        let path = dir.path().join("test_file_open.csv");
+
+        let mut temp_file = File::create(&path).unwrap();
+        temp_file.write_all(b"source_id,title,description,published_at,actual_start_at\n").unwrap();
+
+        // open the file test
+        let file = file_open(path.to_str().unwrap()).unwrap();
+
+        // check if the file is opened
+        assert!(file.metadata().is_ok());
+
+        // cleanup
+        std::fs::remove_file(path).unwrap();
+    }
+
+    #[test]
+    fn test_file_open_failure() {
+        // open the file test
+        let file = file_open("non_existent_file.csv");
+
+        // check if the file is opened
+        assert!(file.is_err());
+    }
+
 }
