4001587b2defc4bba95f7a798233bfbd2da79d8f
KasumiMercury <naoki_shira@outlook.jp>
2024-06-17 22:35:52 +0900

    :construction: add test

--- src-tauri/src/load.rs
+++ src-tauri/src/load.rs
@@ -1,9 +1,31 @@
 use std::fs::File;
+use std::io::Write;
 use anyhow::anyhow;
+use tempfile::TempDir;
 use crate::custom_error::CustomError;
 
-fn file_open(path: &str) -> Result<File, CustomError> {
+pub(crate) fn file_open(path: &str) -> Result<File, CustomError> {
     let file = File::open(path)
         .map_err(|e| CustomError::Anyhow(anyhow!("Failed to open {}: {}", path, e)))?;
     Ok(file)
 }
+
+#[test]
+fn test_file_open() {
+    // create a temporary file
+    let dir = TempDir::new().unwrap();
+    let path = dir.path().join("test_file_open.csv");
+
+    let mut temp_file = File::create(&path).unwrap();
+    temp_file.write_all(b"source_id,title,description,published_at,actual_start_at\n").unwrap();
+
+    // open the file test
+    let file = file_open(path.to_str().unwrap()).unwrap();
+
+    // check if the file is opened
+    assert!(file.metadata().is_ok());
+
+    // cleanup
+    std::fs::remove_file(path).unwrap();
+}
+
