9c10b3b5bbe90a560414af2ccf2b97be6908ede5
KasumiMercury <naoki_shira@outlook.jp>
2024-06-17 22:51:43 +0900

    :construction: remove unused

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::fs::File;
 
 use anyhow::{anyhow, Context as _, Result};
 use dotenvy::dotenv;
