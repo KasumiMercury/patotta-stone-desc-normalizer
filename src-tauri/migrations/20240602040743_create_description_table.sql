382600dbfef93e3249ac28bb4ae1b2136b54fbfd
KasumiMercury <naoki_shira@outlook.jp>
2024-06-26 23:44:08 +0900

    :construction: add desc_hash / to detect duplicate data

--- src-tauri/migrations/20240602040743_create_description_table.sql
+++ src-tauri/migrations/20240602040743_create_description_table.sql
@@ -1,14 +1,20 @@
 -- Add migration script here
 
 -- Create the description table
-CREATE TABLE description (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  source_id TEXT NOT NULL,
-  title TEXT NOT NULL,
-  description TEXT NOT NULL,
-  published_at TEXT NOT NULL,
-  actual_start_at TEXT NOT NULL
+CREATE TABLE description
+(
+    id              INTEGER PRIMARY KEY AUTOINCREMENT,
+    source_id       TEXT NOT NULL,
+    title           TEXT NOT NULL,
+    description     TEXT NOT NULL,
+    desc_hash       TEXT NOT NULL,
+    published_at    TEXT NOT NULL,
+    actual_start_at TEXT NOT NULL
 );
 
 -- Add unique constraint to description table
+
+-- Add unique constraint to source_id column
 CREATE UNIQUE INDEX description_unique ON description (source_id);
+-- Add unique constraint to desc_hash column
+CREATE UNIQUE INDEX desc_hash_unique ON description (desc_hash);
