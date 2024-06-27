6c192080920a33ed25bdf6f11b8682b7fe76de57
KasumiMercury <naoki_shira@outlook.jp>
2024-06-27 22:17:50 +0900

    :construction: get existence_info when launch

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
 import {open} from "@tauri-apps/api/dialog";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import "./App.css";
 import {invoke} from "@tauri-apps/api/tauri";
 
 function App() {
-	const [filePath, setFilePath] = useState("");
 	const [isLoaded, setIsLoaded] = useState(false);
+	const [filePath, setFilePath] = useState("");
 	const [openConfirmDialog, setOpenConfirmDialog] = useState(false);
 	const [error, setError] = useState("");
+
+	// Check the data has already existed in the sqlite database
+	// If the data is already existed, load the data from the database
+	// If the data is not existed, display button to open dialog of file selection
+	useEffect(() => {
+		(async () => {
+			// result is json object
+			const result: string = await invoke("check_data_exists");
+			// parse the json object
+			const existence_info = JSON.parse(result);
+			if (existence_info.exists) {
+				setFilePath(existence_info.path);
+				setIsLoaded(true);
+			} else {
+				setIsLoaded(false);
+			}
+		})();
+	}, []);
 
 	function openLoadDialog() {
 		open({
