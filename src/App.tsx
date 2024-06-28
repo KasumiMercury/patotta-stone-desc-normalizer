55db623b1cfae63b389119c49cfee61f10e40063
KasumiMercury <naoki_shira@outlook.jp>
2024-06-28 19:46:06 +0900

    :construction: remove elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import {open} from "@tauri-apps/api/dialog";
-import {useEffect, useState} from "react";
+import { open } from "@tauri-apps/api/dialog";
+import { useEffect, useState } from "react";
 import "./App.css";
-import {invoke} from "@tauri-apps/api/tauri";
+import { invoke } from "@tauri-apps/api/tauri";
 
 function App() {
 	const [isLoaded, setIsLoaded] = useState(false);
@@ -69,70 +69,7 @@ function App() {
 	}
 
 	return (
-		<div className="m-0 flex justify-center text-center pt-6 px-2 flex-col">
-			<div className="w-full flex justify-end gap-2">
-				<button
-					type="button"
-					className="py-2 px-4 text-xs hover:outline-neutral-100 rounded-md border-2 border-zinc-700 bg-zinc-800"
-					onClick={openLoadDialog}
-				>
-					Load
-				</button>
-				<button
-					type="button"
-					className="py-2 px-4 text-xs hover:outline-neutral-100 rounded-md border-2 border-zinc-700 bg-zinc-800"
-				>
-					Export
-				</button>
-			</div>
-			<div>
-				{error && (
-					<div className="w-full">
-						<p className="text-red-500">{error}</p>
-					</div>
-				)}
-				{isLoaded ? (
-					<div className="w-full">
-						<p>{filePath}</p>
-					</div>
-				) : (
-					<div className="w-full h-96">
-						<div className="flex justify-center items-center h-full">
-							<p className="text-lg">No data</p>
-						</div>
-					</div>
-				)}
-			</div>
-
-			{openConfirmDialog && (
-				<div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center">
-					<div className="bg-zinc-800 p-4 rounded-md border border-zinc-600 gap-y-6 flex flex-col">
-						<div className="flex flex-col w-fit">
-							<p>Do you want to load this file?</p>
-							<p className="text-xs">{filePath}</p>
-						</div>
-						<div className="flex justify-center gap-2">
-							<button
-								type="button"
-								className="py-2 px-4 text-xs hover:outline-neutral-100 rounded-md border-2 border-zinc-700 bg-zinc-800"
-								onClick={loadFile}
-							>
-								Yes
-							</button>
-							<button
-								type="button"
-								className="py-2 px-4 text-xs hover:outline-neutral-100 rounded-md border-2 border-zinc-700 bg-zinc-800"
-								onClick={() => {
-									setOpenConfirmDialog(false);
-									setIsLoaded(false);
-								}}
-							>
-								No
-							</button>
-						</div>
-					</div>
-				</div>
-			)}
+		<div>
 		</div>
 	);
 }
