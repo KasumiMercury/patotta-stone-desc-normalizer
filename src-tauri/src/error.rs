aad1757eff11362939e0991be1152c6d3f551d65
KasumiMercury <naoki_shira@outlook.jp>
2024-06-30 22:33:53 +0900

    :construction: fix error handling

--- src-tauri/src/error.rs
+++ src-tauri/src/error.rs
@@ -1,8 +1,11 @@
 use thiserror::Error;
 use crate::db::db_error::DbError;
+use crate::utils::util_errors::UtilError;
 
 #[derive(Debug, Error)]
 pub enum CustomError {
+    #[error(transparent)]
+    UtilError(#[from] UtilError),
     #[error(transparent)]
     DbError(#[from] DbError),
 }
