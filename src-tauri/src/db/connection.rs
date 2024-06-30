0c24b8a7bb60ab294de90daae4f284d27554c167
KasumiMercury <naoki_shira@outlook.jp>
2024-06-30 21:36:03 +0900

    :construction: refactoring

--- src-tauri/src/db/connection.rs
+++ src-tauri/src/db/connection.rs
@@ -1,8 +1,17 @@
+use std::path::PathBuf;
 use sqlx::SqlitePool;
 use tauri::AppHandle;
 
 use crate::error::CustomError;
 use crate::db::db_error::DbError;
+
+const DB_NAME: &str = "data.db";
+
+fn db_path(mut base: PathBuf) -> String {
+    base.push(DB_NAME);
+
+    format!("sqlite://{}", base.to_str().expect("Failed to get db path"))
+}
 
 pub async fn initialize_sqlite(handle: AppHandle) -> Result<(), CustomError> {
     Ok(())
--- src-tauri/src/utils/path.rs
+++ src-tauri/src/utils/path.rs
@@ -2,15 +2,7 @@ use std::path::PathBuf;
 use tauri::AppHandle;
 use crate::utils::util_errors::UtilError;
 
-const DB_NAME: &str = "data.db";
-
 fn app_path(handle: &AppHandle) -> Result<PathBuf, UtilError> {
     handle.path_resolver().app_data_dir()
         .ok_or(UtilError::AppDataError)
-}
-
-fn db_path(mut base: PathBuf) -> String {
-    base.push(DB_NAME);
-
-    format!("sqlite://{}", base.to_str().expect("Failed to get db path"))
 }
