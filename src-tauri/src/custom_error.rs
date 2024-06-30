28ab5d7257e05e9650b6b32c382535bf660d6fa4
KasumiMercury <naoki_shira@outlook.jp>
2024-06-30 17:16:33 +0900

    :construction: temporary error definition / for backward compatibility

--- src-tauri/src/custom_error.rs
+++ src-tauri/src/custom_error.rs
@@ -5,4 +5,7 @@ use crate::db::db_error::DbError;
 pub enum CustomError {
     #[error("Database error: {0}")]
     DbError(#[from] DbError),
+
+    #[error("Anyhow error: {0}")]
+    Anyhow(#[from] anyhow::Error),
 }
