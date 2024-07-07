1d745019747fd96f4b5ffaf3e27778a4579c75a1
KasumiMercury <naoki_shira@outlook.jp>
2024-07-07 17:47:09 +0900

    :construction: fix custom error

--- src-tauri/src/db/db_error.rs
+++ src-tauri/src/db/db_error.rs
@@ -3,9 +3,9 @@ pub enum DbError {
     #[error("Failed to get database path")]
     PathError,
     #[error("Failed to create database: {0}")]
-    CreateError(#[from] sqlx::Error),
+    CreateError(#[source] sqlx::Error),
     #[error("Failed to migrate database: {0}")]
     MigrateError(#[from] sqlx::migrate::MigrateError),
     #[error("Failed to connect to database: {0}")]
-    ConnectionError(#[from] sqlx::Error),
+    ConnectionError(#[source] sqlx::Error),
 }
