8032cd909e4cf37da6ca22ec9e67b769bc098ceb
KasumiMercury <naoki_shira@outlook.jp>
2024-07-08 15:51:15 +0900

    :construction: custom error

--- src-tauri/src/db/load.rs
+++ src-tauri/src/db/load.rs
@@ -1,6 +1,9 @@
 use sqlx::SqlitePool;
 use tauri::State;
 
+use crate::custom_error::CustomError;
+use crate::db::db_error::DbError;
+
 #[derive(Debug, sqlx::FromRow)]
 struct History {
     id: i32,
@@ -28,13 +31,13 @@ impl History {
     }
 }
 
-async fn get_load_history(pool: State<'_,SqlitePool>) -> Result<Vec<History>,Err()>{
+async fn get_load_history(pool: State<'_,SqlitePool>) -> Result<Vec<History>,CustomError>{
     let p = pool.clone();
     let history = sqlx::query_as::<_, History>(
         r#"
         SELECT * FROM load_history ORDER BY id DESC
         "#,
-    ).fetch_all(&*p).await?;
+    ).fetch_all(&*p).await.map_err(|e| CustomError::DbError(DbError::Query(e)))?;
 
     Ok(history)
 }
