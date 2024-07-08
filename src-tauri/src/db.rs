370fbbd97813cf1857cdffe6477675e79e956a9a
KasumiMercury <naoki_shira@outlook.jp>
2024-07-08 14:35:22 +0900

    :construction: get load history

--- src-tauri/src/db.rs
+++ src-tauri/src/db.rs
@@ -1,2 +1,3 @@
 pub mod connection;
+pub mod load;
 pub mod db_error;
---
+++ src-tauri/src/db/load.rs
@@ -0,0 +1,22 @@
+use sqlx::SqlitePool;
+use tauri::State;
+
+use crate::History;
+
+struct Hisory {
+    pub id: i32,
+    pub path: String,
+    pub count: i32,
+    pub loaded_at: String,
+}
+
+async fn get_load_history(pool: State<'_,SqlitePool>) -> Result<Vec<History>,Err()>{
+    let p = pool.clone();
+    let history = sqlx::query_as::<_, History>(
+        r#"
+        SELECT * FROM load_history ORDER BY id DESC
+        "#,
+    ).fetch_all(&*p).await?;
+
+    Ok(history)
+}
